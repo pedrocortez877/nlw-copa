dea032e5fa6507bbeb6d727c2dab1bc65ad8644a
Pedro Cortez <pedrocortez877@gmail.com>
2022-11-01 09:35:35 -0300

    aula 1

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get('/pools/count', () => {
-    return { count: 0 }
+  await fastify.register(cors, {
+    origin: true,
+  })
+
+  fastify.get('/pools/count', async () => {
+    const pools = await prisma.pool.count();
+
+    return { pools };
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
